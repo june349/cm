3979a3200addaf39677204c05f65cbedf91cb2ac
june349 <lavender125@proton.me>
2024-10-14 04:17:09 -0400

    feat: implement commands

--- src/main.rs
+++ src/main.rs
@@ -40,5 +40,46 @@ enum Command {
 }
 
 fn main() {
-    todo!();
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::From {
+            game,
+            sensitivity,
+            dpi,
+        } => match games::get_game(&game) {
+            Some(game) => {
+                let cm_per_360 = 360.0 / (game.yaw * dpi as f64 * sensitivity) * 2.54;
+                println!("{:.3}", cm_per_360);
+            }
+            None => {
+                eprintln!("Error: Game '{}' not found.", game);
+            }
+        },
+
+        Command::To { game, cm, dpi } => match games::get_game(&game) {
+            Some(game) => {
+                let sensitivity = 360.0 / (game.yaw * dpi as f64 * (cm / 2.54));
+                println!("{:.3}", sensitivity);
+            }
+            None => {
+                eprintln!("Error: Game '{}' not found.", game);
+            }
+        },
+
+        Command::List { aliases } => {
+            for game in games::GAMES {
+                if aliases {
+                    if game.aliases.is_empty() {
+                        println!("{} (Aliases: none)", game.title);
+                    } else {
+                        let alias_list = game.aliases.join(", ");
+                        println!("{} (Aliases: {})", game.title, alias_list);
+                    }
+                } else {
+                    println!("{}", game.title);
+                }
+            }
+        }
+    }
 }
