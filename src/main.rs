cb8cd07d556e82e2ccb53307bbe2310cca3a8b70
june349 <lavender125@proton.me>
2024-10-13 21:58:21 -0400

    feat: create basic cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, arg_required_else_help = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Convert a game sensitivity to cm/360°
+    From {
+        /// The game you're converting from
+        game: String,
+        /// Your sensitivity
+        sensitivity: f64,
+        /// Your DPI
+        dpi: u32,
+    },
+
+    /// Convert cm/360° to a game sensitivity
+    To {
+        /// The game you're converting to
+        game: String,
+        /// Your cm/360°
+        cm: f64,
+        /// Your DPI
+        dpi: u32,
+    },
+
+    /// List supported games
+    List {
+        /// List aliases along with each game (if any)
+        #[arg(short, long)]
+        aliases: bool,
+    },
+}
+
 fn main() {
     todo!();
 }
