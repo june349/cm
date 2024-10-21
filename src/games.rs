65208978ad01aa4e1c4c7d73868efd94ca9d5f2c
june349 <lavender125@proton.me>
2024-10-20 21:57:29 -0400

    feat(games): add valorant, apex legends, & tf2

--- src/games.rs
+++ src/games.rs
@@ -10,7 +10,7 @@ pub fn get_game(query: &str) -> Option<&Game> {
         .find(|&game| game.title == query || game.aliases.iter().any(|&alias| alias == query))
 }
 
-pub const GAMES: &[Game; 2] = &[
+pub const GAMES: &[Game; 5] = &[
     Game {
         title: "Counter-Strike",
         aliases: &["cs", "css", "cs2", "csgo"],
@@ -19,6 +19,21 @@ pub const GAMES: &[Game; 2] = &[
     Game {
         title: "Overwatch",
         aliases: &["ow", "ow2"],
+        yaw: 0.0066,
+    },
+    Game {
+        title: "Valorant",
+        aliases: &["val"],
+        yaw: 0.0066,
+    },
+    Game {
+        title: "Apex Legends",
+        aliases: &["apex"],
+        yaw: 0.0066,
+    },
+    Game {
+        title: "Team Fortress 2",
+        aliases: &["tf2"],
         yaw: 0.0066,
     },
 ];
