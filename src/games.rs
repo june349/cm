c30bd2b0acc6bcca38a40b9e530f10946349c273
june349 <176018493+june349@users.noreply.github.com>
2024-11-13 23:31:03 -0500

    feat: add more games

--- src/games.rs
+++ src/games.rs
@@ -10,7 +10,7 @@ pub fn get_game(query: &str) -> Option<&Game> {
         .find(|&game| game.title == query || game.aliases.iter().any(|&alias| alias == query))
 }
 
-pub const GAMES: &[Game; 5] = &[
+pub const GAMES: &[Game; 11] = &[
     Game {
         title: "Counter-Strike",
         aliases: &["cs", "css", "cs2", "csgo"],
@@ -24,7 +24,7 @@ pub const GAMES: &[Game; 5] = &[
     Game {
         title: "Valorant",
         aliases: &["val"],
-        yaw: 0.07,
+        yaw:  0.06996,
     },
     Game {
         title: "Apex Legends",
@@ -35,5 +35,35 @@ pub const GAMES: &[Game; 5] = &[
         title: "Team Fortress 2",
         aliases: &["tf2"],
         yaw: 0.022,
+    },
+    Game {
+        title: "Rainbow Six: Siege",
+        aliases: &["r6", "r6s", "siege"],
+        yaw: 0.005729577951308232,
+    },
+    Game {
+        title: "Fortnite",
+        aliases: &["fn"],
+        yaw: 0.005555,
+    },
+    Game {
+        title: "Destiny 2",
+        aliases: &["destiny", "d2"],
+        yaw: 0.0066,
+    },
+    Game {
+        title: "Rust",
+        aliases: &["rs"],
+        yaw: 0.1125,
+    },
+    Game {
+        title: "THE FINALS",
+        aliases: &["finals"],
+        yaw: 0.00101545,
+    },
+    Game {
+        title: "CALL OF DUTY",
+        aliases: &["cod"],
+        yaw: 0.0066,
     },
 ];
